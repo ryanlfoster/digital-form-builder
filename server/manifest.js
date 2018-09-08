983385cad296d9c274d769b565597bb8a83497b6
David Stone <davidjamesstone@gmail.com>
2018-09-08 21:03:44 +0100

    Update json model

--- server/manifest.js
+++ server/manifest.js
@@ -5,7 +5,7 @@ const pkg = require('../package.json')
 const { getState, mergeState } = require('./db')
 
 const analyticsAccount = config.analyticsAccount
-const dataFilePath = path.join(__dirname, './govsite.mmo.simple.json')
+const dataFilePath = path.join(__dirname, './govsite.mmo.json')
 const data = require(dataFilePath)
 const playgroundModel = require('./govsite.playground.json')
 
