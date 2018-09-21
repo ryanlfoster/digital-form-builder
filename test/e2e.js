ff84b257d076051b13590fbf6af25b799c94053f
David Stone <davidjamesstone@gmail.com>
2018-09-21 11:34:10 +0100

    Tide up e2e

--- test/e2e.js
+++ test/e2e.js
@@ -5,7 +5,7 @@ const chrome = require('selenium-webdriver/chrome')
 const firefox = require('selenium-webdriver/firefox')
 const getJourneys = require('./get-journeys')
 
-const { By, until, Key } = driver
+const { until } = driver
 const lab = exports.lab = Lab.script()
 
 const browserName = 'chrome'
@@ -14,7 +14,7 @@ const mmoData = require('../server/mmo.json')
 const journeys = getJourneys(mmoData)
 
 console.log(journeys.length)
-console.log(journeys)
+// console.log(journeys)
 
 lab.experiment('Metadata Driven e2e Tests', () => {
   let browser
@@ -22,8 +22,8 @@ lab.experiment('Metadata Driven e2e Tests', () => {
   lab.before(async ({ context }) => {
     browser = await new driver.Builder()
       .forBrowser(browserName)
-      .setChromeOptions(new chrome.Options())
-      .setFirefoxOptions(new firefox.Options())
+      .setChromeOptions(new chrome.Options().headless())
+      .setFirefoxOptions(new firefox.Options().headless())
       .build()
   })
 
@@ -31,79 +31,6 @@ lab.experiment('Metadata Driven e2e Tests', () => {
     await browser.get(baseUrl)
     await browser.wait(until.urlIs(baseUrl))
   })
-
-  async function fillFormField (component) {
-    if (component.children) {
-      if (component.type === 'UkAddressField') {
-        // Clicking the postcode search button without a
-        // query causes the manual entry form to display
-        const find = await browser.findElement(By.css('button.postcode-lookup'))
-        await find.click()
-      }
-
-      await fillForm(component.children.formItems)
-    } else {
-      const fieldData = fake(component, model)
-      // console.log(fieldData)
-      if (component.type === 'DatePartsField') {
-        let el = await browser.findElement({ id: `${component.name}__day` })
-        await el.clear()
-        await el.sendKeys(fieldData.day)
-        el = await browser.findElement({ id: `${component.name}__month` })
-        await el.clear()
-        await el.sendKeys(fieldData.month)
-        el = await browser.findElement({ id: `${component.name}__year` })
-        await el.clear()
-        await el.sendKeys(fieldData.year)
-      } else if (component.type === 'DateTimePartsField') {
-        let el = await browser.findElement({ id: `${component.name}__day` })
-        await el.clear()
-        await el.sendKeys(fieldData.day)
-        el = await browser.findElement({ id: `${component.name}__month` })
-        await el.clear()
-        await el.sendKeys(fieldData.month)
-        el = await browser.findElement({ id: `${component.name}__year` })
-        await el.clear()
-        await el.sendKeys(fieldData.year)
-        el = await browser.findElement({ id: `${component.name}__hour` })
-        await el.clear()
-        await el.sendKeys(fieldData.hour)
-        el = await browser.findElement({ id: `${component.name}__minute` })
-        await el.clear()
-        await el.sendKeys(fieldData.minute)
-      } else {
-        if (component.type === 'SelectField') {
-          const el = await browser.findElement({ id: component.name })
-          await el.findElement(By.css(`option[value='${fieldData}']`)).click()
-        } else if (component.type === 'RadiosField' || component.type === 'YesNoField') {
-          const el = await browser.findElement({ id: `${component.name}-${fieldData}` })
-          await el.click()
-        } else if (component.type === 'DateField') {
-          const el = await browser.findElement({ id: component.name })
-          const iso = fieldData.toISOString()
-          await el.sendKeys(iso.substr(8, 2), iso.substr(5, 2), iso.substr(0, 4))
-        } else if (component.type === 'TimeField') {
-          const el = await browser.findElement({ id: component.name })
-          await el.sendKeys('23:15')
-        } else if (component.type === 'DateTimeField') {
-          const iso = fieldData.toISOString()
-          const el = await browser.findElement({ id: component.name })
-          await el.sendKeys(iso.substr(8, 2), iso.substr(5, 2), iso.substr(0, 4),
-            Key.ARROW_RIGHT, iso.substr(11, 2), iso.substr(14, 2))
-        } else {
-          const el = await browser.findElement({ id: component.name })
-          await el.clear()
-          await el.sendKeys(fieldData)
-        }
-      }
-    }
-  }
-
-  async function fillForm (components) {
-    for (let i = 0; i < components.length; i++) {
-      await fillFormField(components[i])
-    }
-  }
 
   for (let i = 0; i < journeys.length; i++) {
     lab.test(`Journey ${i + 1}`, async () => {
